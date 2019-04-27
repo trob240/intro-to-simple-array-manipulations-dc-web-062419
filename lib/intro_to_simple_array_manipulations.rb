88b2d8ca13660f490fa6a04ab201c603b23dbecc
Trevahr Robertson <trevahr@yahoo.com>
2019-04-27 21:19:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,7 +10,11 @@ def using_pop(array)
   array.pop
 end
 
+<<<<<<< HEAD
 def pop_with_args(array)
+=======
+def pop_wit_args(array)
+>>>>>>> ac2226843daeda3f9d1d766adce3aaf15fbbe023
   array.pop(2)
 end
 
@@ -18,12 +22,20 @@ def using_shift(array)
   array.shift
 end
 
+<<<<<<< HEAD
 def shift_with_args(array)
+=======
+def shift_with_wrgs(array)
+>>>>>>> ac2226843daeda3f9d1d766adce3aaf15fbbe023
   array.shift(2)
 end
 
 def using_concat(array1, array2)
+<<<<<<< HEAD
   array1.concat(array2)
+=======
+  array1 + array2
+>>>>>>> ac2226843daeda3f9d1d766adce3aaf15fbbe023
 end
 
 def using_insert(array, element)
@@ -35,6 +47,7 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
+<<<<<<< HEAD
   array.flatten
 end
 
@@ -45,6 +58,13 @@ end
 
 def using_delete_at(array, integer)
   array.delete_at(integer)
+=======
+  a.flatten
+end
+
+def using_delete(array, string)
+  a.delete(string)
+>>>>>>> ac2226843daeda3f9d1d766adce3aaf15fbbe023
 end
 
 
